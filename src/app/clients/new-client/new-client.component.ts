6cb8c94b47a732c73fc41f2a794ce930a5c8d616
Alexandre Moreira <4lexandre.moreira@gmail.com>
2025-03-23 14:06:44 -0300

    criando servico http do client

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {SERVICES_TOKEN} from '../../services/service.token';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { ICLientService } from '../../services/api-client/clients/iclients.service';
 
 @Component({
   selector: 'app-new-client',
   imports: [],
   templateUrl: './new-client.component.html',
-  styleUrl: './new-client.component.scss'
+  styleUrl: './new-client.component.scss',
+  providers:[
+    {provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService}
+  ]
 })
 export class NewClientComponent {
-
+  constructor(@Inject(SERVICES_TOKEN.HTTP.CLIENT)private readonly httpService: ICLientService){}
 }
